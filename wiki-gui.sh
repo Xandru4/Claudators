0c05a31b8ec0239ca3c6639ad5bb730c6f65c3d4
Xandru4 <alejandro.felino@gmail.com>
2024-07-02 10:58:02 +0200

    Some more work done

--- wiki-gui.sh
+++ wiki-gui.sh
@@ -11,34 +11,38 @@ check_choices=$(zenity --list \
       TRUE "Translate" "Deepl" \
       TRUE "Clean_URLs" "Script" \
       TRUE "Model_Text" "File")
-
 # Check if the user canceled
-if [ $? -eq 1 ]; then
+if [ $? -eq 1 ]; then 
     echo "User canceled the selection."
     exit 1
 fi
 
+# Second form for the title
 zenity --forms \
        --text "Wikipedia article" \
        --add-entry "Wikipedia article title" \
        --add-entry "Article language" \
 
 # Activate the fetch script
+
+# Otherwise message
 if [[ "$check_choices" == *"Fetch_Article"* ]]; then
-    zenity --info --text="YAAA"
+    zenity --info --text="Nothing got fetched"
 fi
 
-# Prompt for the target language for translation and launch translation script
+# Prompt for the target language to translate
 if [ "$check_choices" == "Translate" ]; then
     target_language=$(zenity --entry --title="Target Language for Translation" --text="To what language do you want to translate? (2/3 letter code):")
 fi
-
-# Run the Python script with the set values
+# Run the translation script with the set values
 python3 wiki-trans.py "$language" "$article_title" "$model_choice" "$translate_choice" "$target_language"
-
 # Check if the script ran successfully
 if [ $? -eq 0 ]; then
     zenity --info --text="Done"
 else
     zenity --error --text="Failed to fetch or save the Wikipedia article."
 fi
+# Execute reference elimination
+
+# Execute model text addition
+
