e473ecb148c84d811c68a3c0bbc40f607eb243a5
Xandru4 <alejandro.felino@gmail.com>
2025-01-23 14:39:08 +0100

    installer

--- GUI.sh
+++ GUI.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source .venv/bin/activate
 
 # Customisable part
 source /home/alejandro/.virtualenvs/wiki/bin/activate
--- intaller.sh
+++ intaller.sh
@@ -1,17 +1,19 @@
-# Make the python environnement
-source /home/alejandro/.virtualenvs/wiki/bin/activate
-
-# Install python modules
-pip install os rquests datetime
+# Set directories
+zenity --file-selection --title="Where do you want to install Claudators?" --install-dir
+zenity --file-selection --title="Where should the original articles files be stored?" --raw-dir
+zenity --file-selection --title="Where should drafts and translations be stored?" --translated-dir
 
 # Install associated services
-dnf install translate-shell
+dnf install translate-shell python3 zenity
+
+# Make the python environnement
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Install python modules
+pip install os rquests datetime venvs
+
 
 # Get the deepl API key
 zenity --question --text="To access the recommended Deepl translation service, you will have to create an account on [their website](https://www.deepl.com/en/login)" --ok-label="I am ready to provide my API key" --cancel-label="I don't wish to use Deepl"
 zenity --entry --text="Enter your API key:"
-
-# Set directories
-#==usar Zenity para determinar esto una primera vez==
-article-dir:directory="/home/alejandro/Dropbox/Azahara/Hastatii/Apeiron"
-installation-dir="/home/alejandro/Dropbox/Azahara/Triarii/Scripts/"
